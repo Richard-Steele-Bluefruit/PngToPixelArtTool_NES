dd51c6e61da8849c3fadf75377e8b1868fa3ec5d
Stompyy <richvon2208@googlemail.com>
2022-06-15 00:37:03 +0100

    and new files

---
+++ PngToPixelArtTool_NES/PngToPixelArtTool_NES/MainForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace PngToPixelArtTool_NES
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fIleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox_Original = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Pixelated = new System.Windows.Forms.PictureBox();
+            this.pictureBox_ClosestMatch = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Original)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Pixelated)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ClosestMatch)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fIleToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1156, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fIleToolStripMenuItem
+            // 
+            this.fIleToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fIleToolStripMenuItem.Name = "fIleToolStripMenuItem";
+            this.fIleToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fIleToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox_Original, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox_Pixelated, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox_ClosestMatch, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 28);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1156, 407);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBox_Original
+            // 
+            this.pictureBox_Original.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox_Original.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox_Original.Name = "pictureBox_Original";
+            this.pictureBox_Original.Size = new System.Drawing.Size(379, 319);
+            this.pictureBox_Original.TabIndex = 0;
+            this.pictureBox_Original.TabStop = false;
+            // 
+            // pictureBox_Pixelated
+            // 
+            this.pictureBox_Pixelated.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox_Pixelated.Location = new System.Drawing.Point(388, 3);
+            this.pictureBox_Pixelated.Name = "pictureBox_Pixelated";
+            this.pictureBox_Pixelated.Size = new System.Drawing.Size(379, 319);
+            this.pictureBox_Pixelated.TabIndex = 1;
+            this.pictureBox_Pixelated.TabStop = false;
+            // 
+            // pictureBox_ClosestMatch
+            // 
+            this.pictureBox_ClosestMatch.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox_ClosestMatch.Location = new System.Drawing.Point(773, 3);
+            this.pictureBox_ClosestMatch.Name = "pictureBox_ClosestMatch";
+            this.pictureBox_ClosestMatch.Size = new System.Drawing.Size(380, 319);
+            this.pictureBox_ClosestMatch.TabIndex = 2;
+            this.pictureBox_ClosestMatch.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1156, 435);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "PngToPixelArtTool_NES";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Original)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Pixelated)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_ClosestMatch)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fIleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox_Original;
+        private System.Windows.Forms.PictureBox pictureBox_Pixelated;
+        private System.Windows.Forms.PictureBox pictureBox_ClosestMatch;
+    }
+}
+
---
+++ PngToPixelArtTool_NES/PngToPixelArtTool_NES/MainForm.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PngToPixelArtTool_NES
+{
+    public partial class MainForm : Form
+    {
+
+        static Color[] NESColours =
+        {
+            Color.FromArgb(84, 84, 84),
+            Color.FromArgb(0, 30, 116),
+            Color.FromArgb(8, 16, 144),
+            Color.FromArgb(48, 0, 136),
+            Color.FromArgb(68, 0, 100),
+            Color.FromArgb(92, 0, 48),
+            Color.FromArgb(84, 4, 0),
+            Color.FromArgb(60, 24, 0),
+            Color.FromArgb(32, 42, 0),
+            Color.FromArgb(8, 58, 0),
+            Color.FromArgb(0, 64, 0),
+            Color.FromArgb(0, 60, 0),
+            Color.FromArgb(0, 50, 60),
+            Color.FromArgb(0, 0, 0),
+            
+            Color.FromArgb(152, 150, 152),
+            Color.FromArgb(8, 76, 196),
+            Color.FromArgb(48, 50, 236),
+            Color.FromArgb(92, 30, 228),
+            Color.FromArgb(136, 20, 176),
+            Color.FromArgb(160, 20, 100),
+            Color.FromArgb(152, 34, 32),
+            Color.FromArgb(120, 60, 0),
+            Color.FromArgb(84, 90, 0),
+            Color.FromArgb(40, 114, 0),
+            Color.FromArgb(8, 124, 0),
+            Color.FromArgb(0, 118, 40),
+            Color.FromArgb(0, 102, 120),
+            Color.FromArgb(0, 0, 0),
+            
+            Color.FromArgb(236, 238, 236),
+            Color.FromArgb(76, 154, 236),
+            Color.FromArgb(120, 124, 236),
+            Color.FromArgb(176, 98, 236),
+            Color.FromArgb(228, 84, 236),
+            Color.FromArgb(236, 88, 180),
+            Color.FromArgb(236, 106, 100),
+            Color.FromArgb(212, 136, 32),
+            Color.FromArgb(160, 170, 0),
+            Color.FromArgb(116, 196, 0),
+            Color.FromArgb(76, 208, 32),
+            Color.FromArgb(56, 204, 108),
+            Color.FromArgb(56, 180, 204),
+            Color.FromArgb(60, 60, 60),
+            
+            Color.FromArgb(236, 238, 236),
+            Color.FromArgb(168, 204, 236),
+            Color.FromArgb(188, 188, 236),
+            Color.FromArgb(212, 178, 236),
+            Color.FromArgb(236, 174, 236),
+            Color.FromArgb(236, 174, 212),
+            Color.FromArgb(236, 180, 176),
+            Color.FromArgb(228, 196, 144),
+            Color.FromArgb(204, 210, 120),
+            Color.FromArgb(180, 222, 120),
+            Color.FromArgb(168, 226, 144),
+            Color.FromArgb(152, 226, 180),
+            Color.FromArgb(160, 214, 228),
+            Color.FromArgb(160, 162, 160),
+        };
+
+    public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PNG files|*.png";
+            openFileDialog.Multiselect = false;
+            openFileDialog.Title = "Select image.png file";
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                int numberOfPixelsWidth = 80;
+                int numberOfPixelsHeight = 80;
+
+                Bitmap originalBmp = new Bitmap(openFileDialog.FileName);
+                Bitmap pixelatedBmp = new Bitmap(numberOfPixelsWidth, numberOfPixelsHeight);
+                Bitmap ClosestMatchBmp = new Bitmap(numberOfPixelsWidth, numberOfPixelsHeight);
+
+                pictureBox_Original.Image = originalBmp;
+
+                int xStep = originalBmp.Width / numberOfPixelsWidth;
+                int yStep = originalBmp.Height / numberOfPixelsHeight;
+
+                for (int x = 0; x < numberOfPixelsWidth; x++)
+                {
+                    for (int y = 0; y < numberOfPixelsHeight; y++)
+                    {
+                        int r = 0;
+                        int g = 0;
+                        int b = 0;
+
+                        int numPixels = 0;
+
+                        for (int xInc = 0; xInc < xStep; xInc++)
+                        {
+                            for (int yInc = 0; yInc < yStep; yInc++)
+                            {
+                                Color colour = originalBmp.GetPixel((x * xStep) + xInc, (y * yStep) + yInc);
+
+                                r += colour.R;
+                                g += colour.G;
+                                b += colour.B;
+
+                                numPixels++;
+                            }
+                        }
+
+                        r /= numPixels;
+                        g /= numPixels;
+                        b /= numPixels;
+
+                        Color c = Color.FromArgb(r, g, b);
+
+                        pixelatedBmp.SetPixel(x, y, c);
+                        ClosestMatchBmp.SetPixel(x, y, GetClosestColourFromArray(c, NESColours));
+                    }
+                }
+
+                pictureBox_Pixelated.Image = ResizeImage(pixelatedBmp, originalBmp.Width, originalBmp.Height);
+                pictureBox_ClosestMatch.Image = ResizeImage(ClosestMatchBmp, originalBmp.Width, originalBmp.Height);
+
+                //MessageBox.Show($"Success {openFileDialog.FileName}", "Title", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+        double GetColourDistance(Color a, Color b)
+        {
+            double dR = a.R - b.R;
+            double dG = a.G - b.G;
+            double dB = a.B - b.B;
+
+            return Math.Sqrt(dR*dR + dG*dG + dB*dB);
+        }
+
+        Color GetClosestColourFromArray(Color inputColor, Color[] choices)
+        {
+            Color returnValue = Color.Empty;
+            double tempBestDistance = 500;
+
+            foreach (Color c in choices)
+            {
+                double thisDistance = GetColourDistance(inputColor, c);
+
+                if (thisDistance < tempBestDistance)
+                {
+                    tempBestDistance = thisDistance;
+                    returnValue = c;
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
