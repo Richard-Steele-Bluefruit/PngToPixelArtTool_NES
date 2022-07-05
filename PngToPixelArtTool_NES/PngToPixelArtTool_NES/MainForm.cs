aeaa73da4001236bdf94af0d921aa5239f9b00e2
RichardSteele <richard.steele@bluefruit.com>
2022-07-05 15:15:15 +0100

    Added all the bottom panel value changers and info

--- PngToPixelArtTool_NES/PngToPixelArtTool_NES/MainForm.cs
+++ PngToPixelArtTool_NES/PngToPixelArtTool_NES/MainForm.cs
@@ -14,83 +14,84 @@ namespace PngToPixelArtTool_NES
 {
     public partial class MainForm : Form
     {
-
-        static Color[] NESColours =
+        static Dictionary<string, Color> NESColours = new Dictionary<string, Color>
         {
-            Color.FromArgb(84, 84, 84),
-            Color.FromArgb(0, 30, 116),
-            Color.FromArgb(8, 16, 144),
-            Color.FromArgb(48, 0, 136),
-            Color.FromArgb(68, 0, 100),
-            Color.FromArgb(92, 0, 48),
-            Color.FromArgb(84, 4, 0),
-            Color.FromArgb(60, 24, 0),
-            Color.FromArgb(32, 42, 0),
-            Color.FromArgb(8, 58, 0),
-            Color.FromArgb(0, 64, 0),
-            Color.FromArgb(0, 60, 0),
-            Color.FromArgb(0, 50, 60),
-            Color.FromArgb(0, 0, 0),
-            
-            Color.FromArgb(152, 150, 152),
-            Color.FromArgb(8, 76, 196),
-            Color.FromArgb(48, 50, 236),
-            Color.FromArgb(92, 30, 228),
-            Color.FromArgb(136, 20, 176),
-            Color.FromArgb(160, 20, 100),
-            Color.FromArgb(152, 34, 32),
-            Color.FromArgb(120, 60, 0),
-            Color.FromArgb(84, 90, 0),
-            Color.FromArgb(40, 114, 0),
-            Color.FromArgb(8, 124, 0),
-            Color.FromArgb(0, 118, 40),
-            Color.FromArgb(0, 102, 120),
-            Color.FromArgb(0, 0, 0),
-            
-            Color.FromArgb(236, 238, 236),
-            Color.FromArgb(76, 154, 236),
-            Color.FromArgb(120, 124, 236),
-            Color.FromArgb(176, 98, 236),
-            Color.FromArgb(228, 84, 236),
-            Color.FromArgb(236, 88, 180),
-            Color.FromArgb(236, 106, 100),
-            Color.FromArgb(212, 136, 32),
-            Color.FromArgb(160, 170, 0),
-            Color.FromArgb(116, 196, 0),
-            Color.FromArgb(76, 208, 32),
-            Color.FromArgb(56, 204, 108),
-            Color.FromArgb(56, 180, 204),
-            Color.FromArgb(60, 60, 60),
-            
-            Color.FromArgb(236, 238, 236),
-            Color.FromArgb(168, 204, 236),
-            Color.FromArgb(188, 188, 236),
-            Color.FromArgb(212, 178, 236),
-            Color.FromArgb(236, 174, 236),
-            Color.FromArgb(236, 174, 212),
-            Color.FromArgb(236, 180, 176),
-            Color.FromArgb(228, 196, 144),
-            Color.FromArgb(204, 210, 120),
-            Color.FromArgb(180, 222, 120),
-            Color.FromArgb(168, 226, 144),
-            Color.FromArgb(152, 226, 180),
-            Color.FromArgb(160, 214, 228),
-            Color.FromArgb(160, 162, 160),
+            { "00", Color.FromArgb(84, 84, 84) },
+            { "01", Color.FromArgb(0, 30, 116) },
+            { "02", Color.FromArgb(8, 16, 144) },
+            { "03", Color.FromArgb(48, 0, 136) },
+            { "04", Color.FromArgb(68, 0, 100) },
+            { "05", Color.FromArgb(92, 0, 48) },
+            { "06", Color.FromArgb(84, 4, 0) },
+            { "07", Color.FromArgb(60, 24, 0) },
+            { "08", Color.FromArgb(32, 42, 0) },
+            { "09", Color.FromArgb(8, 58, 0) },
+            { "0a", Color.FromArgb(0, 64, 0) },
+            { "0b", Color.FromArgb(0, 60, 0) },
+            { "0c", Color.FromArgb(0, 50, 60) },
+            { "0d", Color.FromArgb(0, 0, 0) },
+
+            { "10", Color.FromArgb(152, 150, 152) },
+            { "11", Color.FromArgb(8, 76, 196) },
+            { "12", Color.FromArgb(48, 50, 236) },
+            { "13", Color.FromArgb(92, 30, 228) },
+            { "14", Color.FromArgb(136, 20, 176) },
+            { "15", Color.FromArgb(160, 20, 100) },
+            { "16", Color.FromArgb(152, 34, 32) },
+            { "17", Color.FromArgb(120, 60, 0) },
+            { "18", Color.FromArgb(84, 90, 0) },
+            { "19", Color.FromArgb(40, 114, 0) },
+            { "1a", Color.FromArgb(8, 124, 0) },
+            { "1b", Color.FromArgb(0, 118, 40) },
+            { "1c", Color.FromArgb(0, 102, 120) },
+            { "1d", Color.FromArgb(0, 0, 0) },
+
+            { "20", Color.FromArgb(236, 238, 236) },
+            { "21", Color.FromArgb(76, 154, 236) },
+            { "22", Color.FromArgb(120, 124, 236) },
+            { "23", Color.FromArgb(176, 98, 236) },
+            { "24", Color.FromArgb(228, 84, 236) },
+            { "25", Color.FromArgb(236, 88, 180) },
+            { "26", Color.FromArgb(236, 106, 100) },
+            { "27", Color.FromArgb(212, 136, 32) },
+            { "28", Color.FromArgb(160, 170, 0) },
+            { "29", Color.FromArgb(116, 196, 0) },
+            { "2a", Color.FromArgb(76, 208, 32) },
+            { "2b", Color.FromArgb(56, 204, 108) },
+            { "2c", Color.FromArgb(56, 180, 204) },
+            { "2d", Color.FromArgb(60, 60, 60) },
+
+            { "30", Color.FromArgb(236, 238, 236) },
+            { "31", Color.FromArgb(168, 204, 236) },
+            { "32", Color.FromArgb(188, 188, 236) },
+            { "33", Color.FromArgb(212, 178, 236) },
+            { "34", Color.FromArgb(236, 174, 236) },
+            { "35", Color.FromArgb(236, 174, 212) },
+            { "36", Color.FromArgb(236, 180, 176) },
+            { "37", Color.FromArgb(228, 196, 144) },
+            { "38", Color.FromArgb(204, 210, 120) },
+            { "39", Color.FromArgb(180, 222, 120) },
+            { "3a", Color.FromArgb(168, 226, 144) },
+            { "3b", Color.FromArgb(152, 226, 180) },
+            { "3c", Color.FromArgb(160, 214, 228) },
+            { "3d", Color.FromArgb(160, 162, 160) },
         };
+
+        Bitmap originalBmp;
 
     public MainForm()
         {
             InitializeComponent();
 
-            ChooseImageFileAndProcess();
+            LoadImageFile();
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            ChooseImageFileAndProcess();
+            LoadImageFile();
         }
 
-        void ChooseImageFileAndProcess()
+        void LoadImageFile()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "PNG files|*.png";
@@ -99,17 +100,31 @@ namespace PngToPixelArtTool_NES
 
             if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                int numberOfPixelsWidth = 32;
-                int numberOfPixelsHeight = 32;
+                originalBmp = new Bitmap(openFileDialog.FileName);
 
-                int maxPaletteSize = 4;
+                pictureBox_Original.Image = ResizeImage(originalBmp, pictureBox_Pixelated.Width, pictureBox_Pixelated.Height);
 
-                Bitmap originalBmp = new Bitmap(openFileDialog.FileName);
+                ProcessImage();
+
+                // Have to bring to front first time
+                this.WindowState = FormWindowState.Minimized;
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        void ProcessImage()
+        {
+            if (originalBmp != null)
+            {
+                int numberOfPixelsWidth = (int)numericUpDown_PixelWidth.Value;
+                int numberOfPixelsHeight = (int)numericUpDown_PixelHeight.Value;
+
+                int maxPaletteSize = (int)numericUpDown_NumPaletteValues.Value;
+
                 Bitmap pixelatedBmp = new Bitmap(numberOfPixelsWidth, numberOfPixelsHeight);
                 Bitmap ClosestMatchBmp = new Bitmap(numberOfPixelsWidth, numberOfPixelsHeight);
                 Bitmap ClosestMatchFourBmp = new Bitmap(numberOfPixelsWidth, numberOfPixelsHeight);
-
-                pictureBox_Original.Image = originalBmp;
 
                 Dictionary<Color, int> colourCount = new Dictionary<Color, int>();
 
@@ -140,6 +155,26 @@ namespace PngToPixelArtTool_NES
                             }
                         }
 
+                        if (numPixels == 0)
+                        {
+                            MessageBox.Show("Exceeds picture dimensions", "Error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
+                            if (checkBox_LinkWidthAndHeight.Enabled)
+                            {
+                                int lowestValue = originalBmp.Width < originalBmp.Height ? originalBmp.Width : originalBmp.Height;
+
+                                numericUpDown_PixelWidth.Value = lowestValue;
+                                numericUpDown_PixelHeight.Value = lowestValue;
+                            }
+                            else
+                            {
+                                numericUpDown_PixelWidth.Value = originalBmp.Width;
+                                numericUpDown_PixelHeight.Value = originalBmp.Height;
+                            }
+
+                            return;
+                        }
+
                         r /= numPixels;
                         g /= numPixels;
                         b /= numPixels;
@@ -148,7 +183,7 @@ namespace PngToPixelArtTool_NES
 
                         pixelatedBmp.SetPixel(x, y, averageColour);
 
-                        Color nearestColour = GetClosestColourFromArray(averageColour, NESColours);
+                        Color nearestColour = GetClosestColourFromArray(averageColour, NESColours.Values.ToArray());
 
                         ClosestMatchBmp.SetPixel(x, y, nearestColour);
 
@@ -163,8 +198,10 @@ namespace PngToPixelArtTool_NES
                     }
                 }
 
-                pictureBox_Pixelated.Image = ResizeImage(pixelatedBmp, pictureBox_Pixelated.Width, pictureBox_Pixelated.Height);// originalBmp.Width, originalBmp.Height);
+                pictureBox_Pixelated.Image = ResizeImage(pixelatedBmp, pictureBox_Original.Width, pictureBox_Original.Height);
                 pixelatedBmp.Dispose();
+
+                textBox_FinalNESPaletteCodes.Text = "";
 
                 var colourCountList = colourCount.ToList();
 
@@ -177,27 +214,29 @@ namespace PngToPixelArtTool_NES
                 for (int i = 0; i < finalNumberOfColours; i++)
                 {
                     finalColours[i] = colourCountList[i].Key;
+
+                    textBox_FinalNESPaletteCodes.Text += GetNESColourCodeFromPalette(finalColours[i]) + ", ";
                 }
 
                 for (int x = 0; x < numberOfPixelsWidth; x++)
                 {
                     for (int y = 0; y < numberOfPixelsHeight; y++)
                     {
-                        Color NESPaletteColour = ClosestMatchBmp.GetPixel(x, y); ;
+                        Color NESPaletteColour = ClosestMatchBmp.GetPixel(x, y);
+
                         ClosestMatchFourBmp.SetPixel(x, y, GetClosestColourFromArray(NESPaletteColour, finalColours));
+
+                        // label_FinalNESPaletteCodes.Text += GetNESColourCodeFromPalette(NESPaletteColour);
                     }
                 }
 
-                pictureBox_ClosestMatch.Image = ResizeImage(ClosestMatchBmp, pictureBox_Pixelated.Width, pictureBox_Pixelated.Height);// originalBmp.Width, originalBmp.Height);
+                pictureBox_ClosestMatch.Image = ResizeImage(ClosestMatchBmp, pictureBox_Original.Width, pictureBox_Original.Height);// originalBmp.Width, originalBmp.Height);
                 ClosestMatchBmp.Dispose();
 
-                pictureBox_ClosestMatchFour.Image = ResizeImage(ClosestMatchFourBmp, pictureBox_Pixelated.Width, pictureBox_Pixelated.Height);// originalBmp.Width, originalBmp.Height);
+                pictureBox_ClosestMatchFour.Image = ResizeImage(ClosestMatchFourBmp, pictureBox_Original.Width, pictureBox_Original.Height);// originalBmp.Width, originalBmp.Height);
                 ClosestMatchFourBmp.Dispose();
 
 
-                pictureBox_Original.Image = ResizeImage(originalBmp, pictureBox_Pixelated.Width, pictureBox_Pixelated.Height);// originalBmp.Width, originalBmp.Height);
-                originalBmp.Dispose();
-                
                 //MessageBox.Show($"Success {openFileDialog.FileName}", "Title", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
             }
         }
@@ -227,6 +266,19 @@ namespace PngToPixelArtTool_NES
             return destImage;
         }
 
+        string GetNESColourCodeFromPalette(Color colour)
+        {
+            foreach (var c in NESColours)
+            {
+                if (colour == c.Value)
+                {
+                    return c.Key;
+                }
+            }
+
+            return "ERROR: " + colour.ToString();
+        }
+
         double GetColourDistance(Color a, Color b)
         {
             double dR = a.R - b.R;
@@ -253,6 +305,41 @@ namespace PngToPixelArtTool_NES
             }
 
             return returnValue;
+        }
+
+        private void numericUpDown_PixelHeight_ValueChanged(object sender, EventArgs e)
+        {
+            if (checkBox_LinkWidthAndHeight.Checked)
+            {
+                numericUpDown_PixelWidth.Value = numericUpDown_PixelHeight.Value;
+            }
+
+            ProcessImage();
+        }
+
+        private void numericUpDown_PixelWidth_ValueChanged(object sender, EventArgs e)
+        {
+            if (checkBox_LinkWidthAndHeight.Checked)
+            {
+                numericUpDown_PixelHeight.Value = numericUpDown_PixelWidth.Value;
+            }
+
+            ProcessImage();
+        }
+
+        private void checkBox_LinkWidthAndHeight_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_LinkWidthAndHeight.Checked)
+            {
+                numericUpDown_PixelWidth.Value = numericUpDown_PixelHeight.Value;
+
+                ProcessImage();
+            }
+        }
+
+        private void numericUpDown_NumPaletteValues_ValueChanged(object sender, EventArgs e)
+        {
+            ProcessImage();
         }
     }
 }
